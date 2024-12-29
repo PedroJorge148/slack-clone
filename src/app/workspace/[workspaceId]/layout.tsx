5826ff2aeeeab37c1854cfbb12ef4a512d7dba30
Pedro Jorge <pedrojorge2011@gmail.com>
2024-12-29 17:24:57 -0300

    workspace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,15 +1,32 @@
 'use client'
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
-export default function WorkspaceLayout({ children }: { children: React.ReactNode}) {
+export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   )
---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,37 @@
+import { AlertTriangle, Loader2 } from "lucide-react"
+
+import { useCurrentMember } from "@/features/members/api/use-current-member"
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { WorkspaceHeader } from "./workspace-header"
+
+export function WorkspaceSidebar() {
+  const workspaceId = useWorkspaceId()
+
+  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId })
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId })
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader2 className="size-5 animate-spin text-white" />
+      </div>
+    )
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex flex-col gap-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5 text-white" />
+        <p className="text-white text-sm">Workspace not found</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+      <WorkspaceHeader workspace={workspace} isAdmin={member.role === 'admin'} />
+    </div>
+  )
+}
